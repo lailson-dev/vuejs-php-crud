70fc5aee0cee8449e10fbf03b0dbae4e74467e14
Lailson Conceição <lailsoncv@hotmail.com>
2019-02-13 16:37:56 -0300

    Finalização do cadastro de novo usuário

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ var app = new Vue({
 	el: "#app",
 	data: {
 		erroMessage: "",
-		users: []
+		users: [],
+		newUser: { username: "", email: "" }
 	},
 
 	created() {
@@ -13,12 +14,38 @@ var app = new Vue({
 		getAllUser() {
 			let vm = this;
 			axios.get('http://localhost/vuejs-estudos/vue-php-crud/php/api.php?action=read')
-				 .then((res) => {				 	
-				 	if(res.data.error)
-				 		vm.errorMessage = res.data.message;
-				 	else
-				 		vm.users = res.data.users;
-				 });
+			 .then((res) => {				 	
+			 	if(res.data.error)
+			 		vm.errorMessage = res.data.message;
+			 	else
+			 		vm.users = res.data.users;
+			 })
+			 .catch(error => {
+			 	console.log(error);
+			 });
+		},
+
+		saveUser() {
+			let formData = this.appendForm(this.newUser);
+			let vm = this;
+			axios.post('http://localhost/vuejs-estudos/vue-php-crud/php/api.php?action=create', formData)
+			.then((res) => {
+				if(res.data.error)
+					vm.errorMessage = res.data.message;
+				else
+					vm.getAllUser();
+			})
+			.catch(error => {
+				console.log(error);
+			});
+		},
+
+		appendForm(data) {
+		  const form = new FormData()
+		  for ( const key in data ) {
+		    form.append(key, data[key]);
+		  }
+		  return form
 		}
 	}
 });
